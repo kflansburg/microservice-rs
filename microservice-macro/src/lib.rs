f3bdb5b0baf9167b3726b87cfc693307853acac4
Kevin Flansburg <62843110+moose-consulting@users.noreply.github.com>
2020-04-20 13:49:26 -0500

    Update lib.rs

--- microservice-macro/src/lib.rs
+++ microservice-macro/src/lib.rs
@@ -10,12 +10,11 @@ pub fn main(_args: TokenStream, item: TokenStream) -> TokenStream {
     let output = input.sig.output;
     let block = input.block;
     let output = quote! {
-        use microservice::{Signal, microservice_run};
         fn main() #output {
-            fn app_main(logger: Logger, config: Config, signal: Signal) #output {
+            fn app_main(logger: Logger, config: Config, signal: microservice::Signal) #output {
                 #block
             }
-            microservice_run(app_main)
+            microservice::microservice_run(app_main)
         }
     };
     output.into()
